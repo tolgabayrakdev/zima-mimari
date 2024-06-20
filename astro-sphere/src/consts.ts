9724c8b991e067fd324ce06c533d575e3cea5742
tolgabayrakdev <bayraktolga28@gmail.com>
2024-06-20 13:41:23 +0300

    first

---
+++ astro-sphere/src/consts.ts
@@ -0,0 +1,61 @@
+import type { Site, Page, Links, Socials } from "@types"
+
+// Global
+export const SITE: Site = {
+  TITLE: "Umit Dinç",
+  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
+  AUTHOR: "Mark Horn",
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: "Work",
+  DESCRIPTION: "Places I have worked.",
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: "Blog",
+  DESCRIPTION: "Writing on topics I am passionate about.",
+}
+
+// Projects Page 
+export const PROJECTS: Page = {
+  TITLE: "Projects",
+  DESCRIPTION: "Recent projects I have worked on.",
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: "Search",
+  DESCRIPTION: "Search all posts and projects by keyword.",
+}
+
+// Links
+export const LINKS: Links = [
+  { 
+    TEXT: "Home", 
+    HREF: "/", 
+  },
+  { 
+    TEXT: "Projects", 
+    HREF: "/projects", 
+  },
+]
+
+// Socials
+export const SOCIALS: Socials = [
+  { 
+    NAME: "Email",
+    ICON: "email", 
+    TEXT: "kimbuaxel@gmail.com",
+    HREF: "mailto:kimbuaxel@gmail.com",
+  },
+  { 
+    NAME: "Instagram",
+    ICON: "Instagram-x",
+    TEXT: "zimamimaristudyo",
+    HREF: "https://instagram.com/zimamimaristudyo",
+  },
+]
+
